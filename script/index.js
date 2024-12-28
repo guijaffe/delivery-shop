6fc46cee4312c580d8073a5a39b62a071387f24f
alexander <zholobko_an@orlan.in>
2024-12-29 00:56:10 +0300

    modal

---
+++ script/index.js
@@ -0,0 +1,22 @@
+const modal = document.querySelector('.cart-modal__overlay');
+const cartBtn = document.querySelector('#cart-button');
+
+const openModal = () => {
+	modal.classList.add('open');
+}
+const closeModal = () => {
+	modal.classList.remove('open');
+}
+
+cartBtn.addEventListener('click', () => {
+	openModal()
+})
+
+modal.addEventListener('click',(event) => {
+	console.log();
+	if (event.target.classList.contains('cart-modal__overlay') ||
+	event.target.closest('.cart-modal__header--close')
+	) {
+		closeModal()
+	}
+})
